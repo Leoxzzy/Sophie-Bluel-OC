e7324748015b93a7e12497e5dc258048b9494070
Léo <leoxzzy@gmail.com>
2024-06-03 04:05:14 -0400

    Refonte complête de tout le système de formulaire de connexion (login.js, login.html et le style qui va avec) + Suppression du fichier 'visibility.css'.

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,46 +1,16 @@
-
-window.addEventListener('load', () => {
-    document.querySelector('#loginButton').addEventListener('click', () => {
-        let userData = {}
-        userData.email = document.querySelector('#mailInput').value
-        userData.password = document.querySelector('#passwordInput').value
-        loginUser(userData)
-    })
-})
-
-function loginUser(userData) {
-    let params = {
-        method: "POST",
-        headers: { "Content-Type": "application/json; charset=UTF-8" },
-        body: JSON.stringify(userData)
-    }
-
-
-    fetch("http://localhost:5678/api/users/login", params)
-        .then(function (response) {
-            if (response.ok) { return response.json(); } // Vérifier si la réponse est OK (code de statut 200) / Parser la réponse JSON
-            throw new Error("Error retrieving projects: " + response.statusText);
-        })
-        .then(function (data) {
-            localStorage.setItem('token', data.token)
-            window.location.href = "index.html"
-        })
-        .catch(function (error) {
-            notification('Identifiants de connexion incorrects!', 'error')
-        });
-
-}
-
-
+/* Fonction permettant d'afficher une petite notification ayant une animation. */
 function notification(text, type) {
-    if (text === undefined && typeof text !== 'string' || type === undefined && typeof type !== 'string') { console.error('Mauvaise utilisation de la fonction "notification"!') }
+    if (text === null && typeof text !== 'string' || type === null && typeof type !== 'string') {
+        return console.error('Mauvaise utilisation de la fonction "notification"!')
+    }
 
     const notification = document.querySelector('.notification')
     notification.innerHTML = ""
 
     let symbolClass = null
     let symbolColor = null
-    if (type == 'success') { symbolClass = "fa-regular fa-circle-check"; symbolColor = 'green'; } else if (type == 'error') { symbolClass = "fa-solid fa-ban"; symbolColor = 'red'; }
+    if (type == 'success') { symbolClass = "fa-regular fa-circle-check"; symbolColor = 'green'; }
+    else if (type == 'error') { symbolClass = "fa-solid fa-ban"; symbolColor = 'red'; }
 
     let notificationSymbol = document.createElement('i')
     notificationSymbol.className = symbolClass
@@ -55,3 +25,42 @@ function notification(text, type) {
     notification.classList.add('show');
     setTimeout(() => { notification.classList.remove('show'); }, 3000)
 }
+
+
+/* Fonction permettant d'exécuter une requête de connexion à l'API en utilisant les identifiants saisis par l'utilisateur. */
+async function loginUser() {
+    let userData = {}
+    userData.email = document.querySelector('#mailInput').value
+    userData.password = document.querySelector('#passwordInput').value
+
+    let params = {
+        method: "POST",
+        headers: { "Content-Type": "application/json; charset=UTF-8" },
+        body: JSON.stringify(userData)
+    }
+
+    try {
+        const loginRequest = await fetch("http://localhost:5678/api/users/login", params)
+        const loginResponse = await loginRequest.json()
+
+        if (loginResponse.token !== null && loginResponse.token !== undefined) {
+            localStorage.setItem('token', loginResponse.token)
+            window.location.href = "index.html"
+        } else {
+            notification('Identifiants de connexion incorrects!', 'error')
+        }
+
+    } catch(error) {
+        console.error('Une erreur est survenue lors de la connexion:', error)
+    }
+}
+
+
+/* Ecoute de l'événement "submit" du formulaire de connexion. */
+window.addEventListener('load', () => {
+    document.getElementById('loginForm').addEventListener('submit', (event) => {
+        event.preventDefault();
+        loginUser();
+    });
+});
+
